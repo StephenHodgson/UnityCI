ab3b1859f8443e76a4d0b58a84833609650eaa7c
Stephen Hodgson <hodgson.designs@gmail.com>
2020-05-09 17:54:43 -0400

    Added powershell and common tempalte

---
+++ InstallUnityHub.ps1
@@ -0,0 +1,13 @@
+Write-Host "Downloading Unity Hub..."
+
+$url = "https://public-cdn.cloud.unity3d.com/hub/prod/UnityHubSetup.exe";
+$outpath = "$PSScriptRoot/UnityHubSetup.exe"
+
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($url, $outpath)
+
+Write-Host "Download Complete, Starting installation..."
+
+Start-Process -Filepath $outpath -ArgumentList "/S" -Wait -PassThru
+
+Write-Host "Installation Complete!"
