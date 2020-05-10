80c1def4113e126549eba03e9f504d85a38273e1
Stephen Hodgson <hodgson.designs@gmail.com>
2020-05-10 15:53:09 -0400

    added linux and darwin installs
    fixed win install

--- InstallUnityHub.ps1
+++ InstallUnityHub.ps1
@@ -1,25 +1,39 @@
 Write-Host "Downloading Unity Hub..."
 
-$url = "https://public-cdn.cloud.unity3d.com/hub/prod/UnityHubSetup.exe";
-$outpath = "$PSScriptRoot/UnityHubSetup.exe"
+$baseUrl = "https://public-cdn.cloud.unity3d.com/hub/prod/UnityHubSetup";
+$outPath = $PSScriptRoot
 
 $wc = New-Object System.Net.WebClient
-$wc.DownloadFile($url, $outpath)
 
 Write-Host "Download Complete, Starting installation..."
 
 if ((-not $global:PSVersionTable.Platform) -or ($global:PSVersionTable.Platform -eq "Win32NT")) {
+  $wc.DownloadFile("$baseUrl.exe", "$outPath.exe")
   $startProcessArgs = @{
     'FilePath'     = $outpath;
-    'ArgumentList' = @("/S", "/D=$Destination");
+    'ArgumentList' = @("/S");
     'PassThru'     = $true;
     'Wait'         = $true;
   }
 }
-else {
+elseif ($global:PSVersionTable.OS.Contains("Darwin")) {
+  $wc.DownloadFile("$baseUrl.AppImage", "$outPath.AppImage")
+  # Note that $Destination has to be a disk path.
+  # sudo installer -package $Package.Path -target
   $startProcessArgs = @{
     'FilePath'     = 'sudo';
-    'ArgumentList' = @("installer", "-package", $outpath);
+    'ArgumentList' = @("installer", "-package", $outpath, "-target", "/Applications/UnityHub");
+    'PassThru'     = $true;
+    'Wait'         = $true;
+  }
+}
+elseif ($global:PSVersionTable.OS.Contains("Linux")) {
+  $wc.DownloadFile("$baseUrl.dmg", "$outPath.dmg")
+  #https://www.linuxdeveloper.space/install-unity-linux/
+  #sudo chmod +x UnityHub.AppImage
+  $startProcessArgs = @{
+    'FilePath'     = 'sudo';
+    'ArgumentList' = @("chmod", "+x", $outPath);
     'PassThru'     = $true;
     'Wait'         = $true;
   }
