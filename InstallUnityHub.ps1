b36cd29447993648d53d9fde3b6744d78855612b
Stephen Hodgson <hodgson.designs@gmail.com>
2020-05-10 23:30:50 -0400

    kill the noise

--- InstallUnityHub.ps1
+++ InstallUnityHub.ps1
@@ -44,41 +44,46 @@ elseif ($global:PSVersionTable.OS.Contains("Darwin")) {
     exit 1
   }
 
-  $dmgVolume = Get-ChildItem -Path "/Volumes" | Select-String -Pattern "/Volumes/Unity Hub"
-  $dmgApp = Get-ChildItem $dmgVolume | Select-String -Pattern ".app"
+  Get-ChildItem -Path "/Volumes"
 
-  Write-Host $dmgVolume
-  Write-Host $dmgApp
+  #Select-String -Pattern "/Volumes/Unity Hub"
 
-  #sudo cp -R /Volumes/<image>\ <image>.app /Applications
-  $startProcessArgs = @{
-    'FilePath'     = 'sudo';
-    'ArgumentList' = @("cp", "-R", "$dmgVolume", "$dmgApp", "/Applications");
-    'PassThru'     = $true;
-    'Wait'         = $true;
-  }
+  #Write-Host $dmgVolume
 
-  $process = Start-Process @startProcessArgs
+  # $dmgApp = Get-ChildItem $dmgVolume | Select-String -Pattern ".app"
+  # Write-Host $dmgApp
 
-  if ( $process.ExitCode -ne 0) {
-    Write-Error "$(Get-Date): Failed with exit code: $($process.ExitCode)"
-    exit 1
-  }
+  # #TODO Check if installed
 
-  #sudo hdiutil detach /Volumes/<image>
-  $startProcessArgs = @{
-    'FilePath'     = 'sudo';
-    'ArgumentList' = @("hdiutil", "detach", "/Volumes/$image");
-    'PassThru'     = $true;
-    'Wait'         = $true;
-  }
+  # #sudo cp -R /Volumes/<image>\ <image>.app /Applications
+  # $startProcessArgs = @{
+  #   'FilePath'     = 'sudo';
+  #   'ArgumentList' = @("cp", "-R", "$dmgVolume", "$dmgApp", "/Applications");
+  #   'PassThru'     = $true;
+  #   'Wait'         = $true;
+  # }
 
-  $process = Start-Process @startProcessArgs
+  # $process = Start-Process @startProcessArgs
 
-  if ( $process.ExitCode -ne 0) {
-    Write-Error "$(Get-Date): Failed with exit code: $($process.ExitCode)"
-    exit 1
-  }
+  # if ( $process.ExitCode -ne 0) {
+  #   Write-Error "$(Get-Date): Failed with exit code: $($process.ExitCode)"
+  #   exit 1
+  # }
+
+  # #sudo hdiutil detach /Volumes/<image>
+  # $startProcessArgs = @{
+  #   'FilePath'     = 'sudo';
+  #   'ArgumentList' = @("hdiutil", "detach", "/Volumes/$image");
+  #   'PassThru'     = $true;
+  #   'Wait'         = $true;
+  # }
+
+  # $process = Start-Process @startProcessArgs
+
+  # if ( $process.ExitCode -ne 0) {
+  #   Write-Error "$(Get-Date): Failed with exit code: $($process.ExitCode)"
+  #   exit 1
+  # }
 }
 elseif ($global:PSVersionTable.OS.Contains("Linux")) {
   #https://www.linuxdeveloper.space/install-unity-linux/
