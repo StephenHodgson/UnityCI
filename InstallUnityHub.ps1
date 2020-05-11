ddc0bc981f957728e1454818cfea2bb323dfe74f
Stephen Hodgson <hodgson.designs@gmail.com>
2020-05-11 13:25:04 -0400

    dual outputs

--- InstallUnityHub.ps1
+++ InstallUnityHub.ps1
@@ -29,9 +29,10 @@ if ((-not $global:PSVersionTable.Platform) -or ($global:PSVersionTable.Platform 
     $hubPath = "C:\Program Files\Unity Hub\Unity Hub.exe"
 
     #"Unity Hub.exe" -- --headless help
-    #. 'C:\Program Files\Unity Hub\Unity Hub.exe' -- --headless help
-    $output = (pwsh -NoLogo -NonInteractive -NoProfile -Command "'""C:\Program Files\Unity Hub\Unity Hub.exe"" -- --headless help'")
+
+    $output = (pwsh -NoLogo -NonInteractive -NoProfile -Command "'C:\Program Files\Unity Hub\Unity Hub.exe -- --headless help'")
     Write-Host $output
+    . "'C:\Program Files\Unity Hub\Unity Hub.exe -- --headless help'"
   }
   else
   {
@@ -45,22 +46,20 @@ elseif ($global:PSVersionTable.OS.Contains("Darwin")) {
   $wc.DownloadFile("$baseUrl/$package", $downloadPath)
 
   $dmgVolume = (sudo hdiutil attach $downloadPath -nobrowse) | Select-String -Pattern '\/Volumes\/.*' -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object { $_.Value } | select-object -first 1
-
   Write-Host $dmgVolume
-
   $dmgAppPath = (find "$DMGVolume" -name "*.app" -depth 1)
-
   Write-Host $dmgAppPath
-
   sudo cp -rvf "`"$dmgAppPath`"" "/Applications"
-
   hdiutil unmount $dmgVolume
 
   $hubPath = "/Applications/Unity\ Hub.app/Contents/MacOS/Unity\ Hub"
 
   # /Applications/Unity\ Hub.app/Contents/MacOS/Unity\ Hub -- --headless help
-  $output = (pwsh -NoLogo -NonInteractive -NoProfile -Command "'""/Applications/Unity\ Hub.app/Contents/MacOS/Unity\ Hub"" -- --headless help'")
+
+  $output = (pwsh -NoLogo -NonInteractive -NoProfile -Command "'/Applications/Unity\ Hub.app/Contents/MacOS/Unity\ Hub -- --headless help'")
   Write-Host $output
+
+  . "'/Applications/Unity\ Hub.app/Contents/MacOS/Unity\ Hub -- --headless help'"
 }
 elseif ($global:PSVersionTable.OS.Contains("Linux")) {
   #https://www.linuxdeveloper.space/install-unity-linux/
@@ -69,8 +68,11 @@ elseif ($global:PSVersionTable.OS.Contains("Linux")) {
   $hubPath = "Unity\ Hub.AppImage"
 
   # Unity\ Hub.AppImage -- --headless help
-  $output = (pwsh -NoLogo -NonInteractive -NoProfile -Command "'""Unity\ Hub.AppImage"" -- --headless help'")
+
+  $output = (pwsh -NoLogo -NonInteractive -NoProfile -Command "'Unity\ Hub.AppImage -- --headless help'")
   Write-Host $output
+
+  . "'Unity\ Hub.AppImage -- --headless help'"
 }
 
 Write-Host "Install Complete: $hubPath"
