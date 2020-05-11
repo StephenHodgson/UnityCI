88881f42655d50334ea378f1a08d3ecc4e8c6871
Stephen Hodgson <hodgson.designs@gmail.com>
2020-05-10 23:25:12 -0400

    try to get dmg volume and app

--- InstallUnityHub.ps1
+++ InstallUnityHub.ps1
@@ -32,7 +32,7 @@ elseif ($global:PSVersionTable.OS.Contains("Darwin")) {
   #sudo hdiutil attach <image>.dmg
   $startProcessArgs = @{
     'FilePath'     = 'sudo';
-    'ArgumentList' = @("hdiutil", "attach", $downloadPath);
+    'ArgumentList' = @("hdiutil", "attach", $downloadPath, "-nobrowse");
     'PassThru'     = $true;
     'Wait'         = $true;
   }
@@ -44,10 +44,16 @@ elseif ($global:PSVersionTable.OS.Contains("Darwin")) {
     exit 1
   }
 
-  #sudo installer -package /Volumes/<image>/<image>.pkg -target /
+  $dmgVolume = Get-ChildItem -Path "/Volumes" | Select-String -Pattern "/Volumes/Unity Hub"
+  $dmgApp = Get-ChildItem $dmgVolume | Select-String -Pattern ".app"
+
+  Write-Host $dmgVolume
+  Write-Host $dmgApp
+
+  #sudo cp -R /Volumes/<image>\ <image>.app /Applications
   $startProcessArgs = @{
     'FilePath'     = 'sudo';
-    'ArgumentList' = @("installer", "-package", "/Volumes/$image/$package", "-target", "/Applications");
+    'ArgumentList' = @("cp", "-R", "$dmgVolume", "$dmgApp", "/Applications");
     'PassThru'     = $true;
     'Wait'         = $true;
   }
@@ -73,7 +79,6 @@ elseif ($global:PSVersionTable.OS.Contains("Darwin")) {
     Write-Error "$(Get-Date): Failed with exit code: $($process.ExitCode)"
     exit 1
   }
-
 }
 elseif ($global:PSVersionTable.OS.Contains("Linux")) {
   #https://www.linuxdeveloper.space/install-unity-linux/
