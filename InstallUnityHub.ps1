aaef8e5ef8abd4fae86d1597097bf3df09a56af1
Stephen Hodgson <hodgson.designs@gmail.com>
2020-06-06 01:36:24 -0400

    Added changeset

--- InstallUnityHub.ps1
+++ InstallUnityHub.ps1
@@ -71,7 +71,7 @@ Write-Host "Unity HUB CLI Options:"
 . $hubPath -- --headless help
 
 Write-Host "Starting Editor Install..."
-. $hubPath -- --headless install --version 2019.1.14f1 --changeset
+. $hubPath -- --headless install --version 2019.1.14f1 --changeset 148b5891095a
 
 Write-Host "Starting Editor Module Install..."
 . $hubPath -- --headless install --version 2019.1.14f1 -m windows
