0ae08a025dba5d1f55b2930a802ae563753bd903
Stephen Hodgson <hodgson.designs@gmail.com>
2020-06-06 18:54:35 -0400

    different quotes?

--- InstallUnityHub.ps1
+++ InstallUnityHub.ps1
@@ -70,17 +70,17 @@ elseif ($global:PSVersionTable.OS.Contains("Linux")) {
   file ./UnityHub.AppImage
 
   # Test inline calls
-  bash -c `""$hubPath -- --headless help`""
+  bash -c ./UnityHub.AppImage -- --headless help
 }
 
 Write-Host "Install Hub Complete: $hubPath"
 Write-Host ""
 Write-Host "Unity HUB CLI Options:"
-$p = Start-Process -FilePath `"""$hubPath`""" -ArgumentList "-- --headless help" -Verbose -NoNewWindow -PassThru -Wait
+$p = Start-Process -FilePath `'"$hubPath`'" -ArgumentList "-- --headless help" -Verbose -NoNewWindow -PassThru -Wait
 Write-Host ""
-$p = Start-Process -FilePath `""$hubPath`"" -ArgumentList "-- --headless install --version 2019.1.14f1 --changeset 148b5891095a" -Verbose -NoNewWindow -PassThru -Wait
+$p = Start-Process -FilePath `'"$hubPath`'" -ArgumentList "-- --headless install --version 2019.1.14f1 --changeset 148b5891095a" -Verbose -NoNewWindow -PassThru -Wait
 Write-Host ""
-$p = Start-Process -FilePath `""$hubPath`"" -ArgumentList "-- --headless editors -i" -Verbose -NoNewWindow -PassThru -Wait
+$p = Start-Process -FilePath `'"$hubPath`'" -ArgumentList "-- --headless editors -i" -Verbose -NoNewWindow -PassThru -Wait
 
 #TODO Get editor installation path and search modules.json for a list of all valid modules available then download them all
 
