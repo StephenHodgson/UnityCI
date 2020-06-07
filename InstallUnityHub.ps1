e72a3da8162306730ef2d504c207f82f5881c3fb
Stephen Hodgson <hodgson.designs@gmail.com>
2020-06-06 22:22:18 -0400

    try something else

--- InstallUnityHub.ps1
+++ InstallUnityHub.ps1
@@ -60,14 +60,12 @@ elseif ($global:PSVersionTable.OS.Contains("Linux")) {
   chmod -v +x "$outPath/UnityHub.AppImage"
   # UnityHub.AppImage -- --headless help
   $hubPath = "$outPath/UnityHub.AppImage"
-  sudo apt install libgconf-2-4
-
   cd $outPath -v
 
-  # Accept License
-  sudo './UnityHub.AppImage'
-
   file ./UnityHub.AppImage
+
+  # Accept License
+  './UnityHub.AppImage'
 
   # Test inline calls
   './UnityHub.AppImage' -- '--headless help'
