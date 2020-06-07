301a71f41a86146139ada199363d78c1f81a6df9
Stephen Hodgson <hodgson.designs@gmail.com>
2020-06-07 15:16:04 -0400

    exclude powershell from linux

--- Install-Hub.sh
+++ Install-Hub.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+echo "Installing unity hub..."
+wget https://public-cdn.cloud.unity3d.com/hub/prod/UnityHub.AppImage
+sudo chmod -v a+x UnityHub.AppImage
 ./UnityHub.AppImage -- --headless help
 ./UnityHub.AppImage -- --headless install --version 2019.1.14f1 --changeset 148b5891095a
