16612b56a278e473ed85b25dec7ad750df771100
Stephen Hodgson <hodgson.designs@gmail.com>
2020-06-07 14:17:03 -0400

    idk anymore lol

--- InstallUnityHub.ps1
+++ InstallUnityHub.ps1
@@ -76,7 +76,8 @@ elseif ($global:PSVersionTable.OS.Contains("Linux")) {
 
   # Accept License
   ./UnityHub.AppImage
-  ./UnityHub.AppImage "\`"-- --headless help\`""
+
+  & ./UnityHub.AppImage --% -- --headless help
 }
 
 Write-Host "Install Hub Complete: $hubPath"
