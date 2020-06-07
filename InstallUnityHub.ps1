24330a676ef05b2f7951fea733469814505aca68
Stephen Hodgson <hodgson.designs@gmail.com>
2020-06-06 20:56:16 -0400

    try a different command type

--- InstallUnityHub.ps1
+++ InstallUnityHub.ps1
@@ -70,7 +70,7 @@ elseif ($global:PSVersionTable.OS.Contains("Linux")) {
   file ./UnityHub.AppImage
 
   # Test inline calls
-  ./UnityHub.AppImage -- --headless help
+  &/UnityHub.AppImage -- --headless help
 }
 
 Write-Host "Install Hub Complete: $hubPath"
