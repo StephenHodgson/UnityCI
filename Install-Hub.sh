61d7ebddafe8a18ea5498b8515a744dc11e316ec
Stephen Hodgson <hodgson.designs@gmail.com>
2020-06-07 15:46:16 -0400

    slightly different

--- Install-Hub.sh
+++ Install-Hub.sh
@@ -3,5 +3,5 @@ echo "Installing unity hub..."
 wget https://public-cdn.cloud.unity3d.com/hub/prod/UnityHub.AppImage
 sudo chmod -v a+x UnityHub.AppImage
 ./UnityHub.AppImage
-./UnityHub.AppImage/unityhub -- --headless help
-./UnityHub.AppImage/unityhub -- --headless install --version 2019.1.14f1 --changeset 148b5891095a
+exec ./UnityHub.AppImage/unityhub "--","--headless","help"
+exec ./UnityHub.AppImage/unityhub "--","--headless","install","--version","2019.1.14f1","--changeset","148b5891095a"
