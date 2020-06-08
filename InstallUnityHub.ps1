4e7a42058ea44cd8b1f673610c76be14f4be3529
Stephen Hodgson <hodgson.designs@gmail.com>
2020-06-07 22:50:40 -0400

    try it a different way

--- InstallUnityHub.ps1
+++ InstallUnityHub.ps1
@@ -4,17 +4,9 @@ $baseUrl = "https://public-cdn.cloud.unity3d.com/hub/prod";
 $outPath = $PSScriptRoot
 $EditorRoot = ""
 $version = "m_EditorVersionWithRevision: 2019.1.14f1 (148b5891095a)"
-
-if( $version -match '(?<version>(?:(?<major>\d+)\.)?(?:(?<minor>\d+)\.)?(?:(?<patch>\d+[fab]\d+)\b))|((?:\((?<revision>\w+))\))' )
-{
-  $UnityVersion = $Matches.version
-  $UnityVersionChangeSet = $Matches.revision
-}
-else
-{
-  Write-Error "Failed to find a valid unity version!"
-  exit 1
-}
+$matches = $version | Select-String  '(?<version>(?:(?<major>\d+)\.)?(?:(?<minor>\d+)\.)?(?:(?<patch>\d+[fab]\d+)\b))|((?:\((?<revision>\w+))\))' -AllMatches
+$UnityVersion = $matches.Matches.groups | ? { $_.Name -eq 'version' } | Select-Object -ExpandProperty Value
+$UnityVersionChangeSet = $matches.Matches.groups | ? { $_.Name -eq 'revision' } | Select-Object -ExpandProperty Value
 
 Write-Host $UnityVersion
 Write-Host $UnityVersionChangeSet
