28e782bcacd2572b030991dd3e24a70fa585dd5e
Stephen Hodgson <hodgson.designs@gmail.com>
2020-06-07 23:48:10 -0400

    get the value

--- InstallUnityHub.ps1
+++ InstallUnityHub.ps1
@@ -6,9 +6,9 @@ $editorPath = ""
 $version = "m_EditorVersionWithRevision: 2019.1.14f1 (148b5891095a)"
 $pattern = '(?<version>(?:(?<major>\d+)\.)?(?:(?<minor>\d+)\.)?(?:(?<patch>\d+[fab]\d+)\b))|((?:\((?<revision>\w+))\))'
 $matches = $matches = [regex]::Matches($version, $pattern)
-$UnityVersion = $matches[0].Groups['version']
+$UnityVersion = $matches[0].Groups['version'].Value.Trim()
 $UnityVersion = $UnityVersion.Trim()
-$UnityVersionChangeSet = $matches[0].Groups['revision']
+$UnityVersionChangeSet = $matches[0].Groups['revision'].Value.Trim()
 $UnityVersionChangeSet = $UnityVersionChangeSet.Trim()
 
 Write-Host $UnityVersion $UnityVersionChangeSet
