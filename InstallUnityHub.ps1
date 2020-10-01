d6f5b56b60e512532f8c55220919e4b950d0691c
Stephen Hodgson <hodgson.designs@gmail.com>
2020-10-01 11:18:17 -0400

    renamed variable to be different from global name set by powershell regex

--- InstallUnityHub.ps1
+++ InstallUnityHub.ps1
@@ -6,9 +6,9 @@ $editorPath = ""
 $editorFileEx = ""
 $version = "m_EditorVersionWithRevision: 2019.1.14f1 (148b5891095a)"
 $pattern = '(?<version>(?:(?<major>\d+)\.)?(?:(?<minor>\d+)\.)?(?:(?<patch>\d+[fab]\d+)\b))|((?:\((?<revision>\w+))\))'
-$matches = $matches = [regex]::Matches($version, $pattern)
-$UnityVersion = $matches[0].Groups['version'].Value.Trim()
-$UnityVersionChangeSet = $matches[1].Groups['revision'].Value.Trim()
+$versonMatches = [regex]::Matches($version, $pattern)
+$UnityVersion = $versonMatches[0].Groups['version'].Value.Trim()
+$UnityVersionChangeSet = $versonMatches[1].Groups['revision'].Value.Trim()
 
 $wc = New-Object System.Net.WebClient
 
