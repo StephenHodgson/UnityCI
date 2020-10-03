4d7e44c073c669294efea2c155f25c8c6a5d7c87
Stephen Hodgson <hodgson.designs@gmail.com>
2020-10-02 21:37:25 -0400

    Test PR with latest Hub (#3)
    * fixed a bit of formatting
    
    * renamed variable to be different from global name set by powershell regex
    
    * clearer success check
    
    updated usages to powershell equivelents
    
    * test error code and message (if any)
    
    * remove error message
    
    * revert
    
    * removed misc line

--- InstallUnityHub.ps1
+++ InstallUnityHub.ps1
@@ -6,9 +6,9 @@ $editorPath = ""
 $editorFileEx = ""
 $version = "m_EditorVersionWithRevision: 2019.1.14f1 (148b5891095a)"
 $pattern = '(?<version>(?:(?<major>\d+)\.)?(?:(?<minor>\d+)\.)?(?:(?<patch>\d+[fab]\d+)\b))|((?:\((?<revision>\w+))\))'
-$matches = $matches = [regex]::Matches($version, $pattern)
-$UnityVersion = $matches[0].Groups['version'].Value.Trim()
-$UnityVersionChangeSet = $matches[1].Groups['revision'].Value.Trim()
+$versonMatches = [regex]::Matches($version, $pattern)
+$UnityVersion = $versonMatches[0].Groups['version'].Value.Trim()
+$UnityVersionChangeSet = $versonMatches[1].Groups['revision'].Value.Trim()
 
 $wc = New-Object System.Net.WebClient
 
@@ -64,7 +64,7 @@ elseif ($global:PSVersionTable.OS.Contains("Darwin")) {
 elseif ($global:PSVersionTable.OS.Contains("Linux")) {
   #https://www.linuxdeveloper.space/install-unity-linux/
   $wc.DownloadFile("$baseUrl/UnityHub.AppImage", "$outPath/UnityHub.AppImage")
-  cd $outPath
+  Set-Location $outPath
   sudo chmod -v a+x UnityHub.AppImage
 
   # UnityHub.AppImage -- --headless help
@@ -83,22 +83,21 @@ Write-Host ""
 Write-Host "Unity HUB CLI Options:"
 $p = Start-Process -Verbose -NoNewWindow -PassThru -Wait -FilePath "$hubPath" -ArgumentList @('--','--headless','help')
 Write-Host ""
-Write-Host "Success? " ($p.ExitCode -eq 0)
-
+Write-Host "Successful exit code? " ($p.ExitCode -eq 0)
 Write-Host ""
 $p = Start-Process -Verbose -NoNewWindow -PassThru -Wait -FilePath "$hubPath" -ArgumentList @('--','--headless','install',"--version $UnityVersion","--changeset $UnityVersionChangeSet")
 Write-Host ""
-Write-Host "Success? " ($p.ExitCode -eq 0)
+Write-Host "Successful exit code? " ($p.ExitCode -eq 0)
 Write-Host ""
 $p = Start-Process -Verbose -NoNewWindow -PassThru -Wait -FilePath "$hubPath" -ArgumentList @('--','--headless','editors','-i')
 Write-Host ""
-Write-Host "Success? " ($p.ExitCode -eq 0)
+Write-Host "Successful exit code? " ($p.ExitCode -eq 0)
 
 $modulesPath = "$editorPath$UnityVersion"
 $editorPath = '{0}{1}{2}' -f $modulesPath,[IO.Path]::DirectorySeparatorChar,$editorFileEx
 
 if ( -not (Test-Path -Path $editorPath) ) {
-  Write-Error "Failed to validate installed editor path at "$editorPath
+  Write-Error "Failed to validate installed editor path at $editorPath"
   exit 1
 }
 
@@ -120,17 +119,17 @@ if ( Test-Path -Path $modulesPath ) {
     Write-Host ""
     $p = Start-Process -Verbose -NoNewWindow -PassThru -Wait -FilePath "$hubPath" -ArgumentList $modules
     Write-Host ""
-    Write-Host "Success? " ($p.ExitCode -eq 0)
+    Write-Host "Successful exit code? " ($p.ExitCode -eq 0)
   } else {
     Write-Error "Failed to resolve modules path at $modulesPath"
     exit 1
   }
 } else {
-  Write-Error "Failed to resolve editor installation path at" $modulesPath
+  Write-Error "Failed to resolve editor installation path at $modulesPath"
   exit 1
 }
 
 Write-Host "Install Complete!"
 Write-Host "UnityEditor path set to: $editorPath"
-echo "##vso[task.setvariable variable=EditorPath]$editorPath"
+Write-Output "##vso[task.setvariable variable=EditorPath]$editorPath"
 exit 0
