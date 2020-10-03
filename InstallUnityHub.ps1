4c8ba83e970ec84732c65e89ead31e09b242f3bb
Stephen Hodgson <hodgson.designs@gmail.com>
2020-10-03 00:04:05 -0400

    put some quotes back

--- InstallUnityHub.ps1
+++ InstallUnityHub.ps1
@@ -66,10 +66,10 @@ elseif ($global:PSVersionTable.OS.Contains("Linux")) {
   $wc.DownloadFile("$baseUrl/UnityHub.AppImage", "/tmp/UnityHub.AppImage")
   sudo chmod -v a+x /tmp/UnityHub.AppImage
   sudo /tmp/UnityHub.AppImage --appimage-extract
-  sudo cp -Rv /tmp/squashfs-root/* /
-  sudo rm -rfv /tmp/squashfs-root /tmp/UnityHub.AppImage
-  sudo mkdir -pv /opt/unity
-  sudo mv /AppRun /opt/unity/UnityHub
+  sudo cp -Rv "/tmp/squashfs-root/*" "/"
+  sudo rm -rfv "/tmp/squashfs-root" "/tmp/UnityHub.AppImage"
+  sudo mkdir -pv "/opt/unity"
+  sudo mv "/AppRun" "/opt/unity/UnityHub"
   sudo mkdir -pv "/root/.config/Unity Hub"
   sudo touch "/root/.config/Unity Hub/eulaAccepted"
 
