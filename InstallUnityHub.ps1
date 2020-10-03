3fccedb021885ad2fa1df4172cc267e183e15fe5
Stephen Hodgson <hodgson.designs@gmail.com>
2020-10-03 00:00:17 -0400

    do we even really need to change directories?

--- InstallUnityHub.ps1
+++ InstallUnityHub.ps1
@@ -65,7 +65,6 @@ elseif ($global:PSVersionTable.OS.Contains("Linux")) {
   #https://www.linuxdeveloper.space/install-unity-linux/
   $wc.DownloadFile("$baseUrl/UnityHub.AppImage", "/tmp/UnityHub.AppImage")
   sudo chmod -v a+x /tmp/UnityHub.AppImage
-  cd -v "/tmp"
   sudo /tmp/UnityHub.AppImage --appimage-extract
   sudo cp -Rv /tmp/squashfs-root/* /
   sudo rm -rfv /tmp/squashfs-root /tmp/UnityHub.AppImage
