dfd51aa49a69fec9454bdcce53cfe5765371a955
Stephen Hodgson <hodgson.designs@gmail.com>
2020-11-16 19:23:54 -0500

    remove xhost stuff

--- InstallUnityHub.ps1
+++ InstallUnityHub.ps1
@@ -62,8 +62,6 @@ elseif ($global:PSVersionTable.OS.Contains("Darwin")) {
   #. "/Applications/Unity Hub.app/Contents/MacOS/Unity Hub" -- --headless help
 }
 elseif ($global:PSVersionTable.OS.Contains("Linux")) {
-  sudo xhost +
-  export DISPLAY=0:0
   sudo mkdir -p /opt/unity/UnityHub
   $hubPath = "/opt/unity/UnityHub.AppImage"
   $editorPath = "~/Unity/Hub/Editor/"
