72377894a70c46f50e4f39bd62dc242a15670f0a
Stephen Hodgson <hodgson.designs@gmail.com>
2020-11-16 19:42:35 -0500

    this won't work anyway

--- InstallUnityHub.ps1
+++ InstallUnityHub.ps1
@@ -62,8 +62,6 @@ elseif ($global:PSVersionTable.OS.Contains("Darwin")) {
   #. "/Applications/Unity Hub.app/Contents/MacOS/Unity Hub" -- --headless help
 }
 elseif ($global:PSVersionTable.OS.Contains("Linux")) {
-  xhost +
-  echo $DISPLAY
   sudo mkdir -p /opt/unity/UnityHub
   $hubPath = "/opt/unity/UnityHub.AppImage"
   $editorPath = "~/Unity/Hub/Editor/"
