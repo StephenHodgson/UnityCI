4aff9a82a611516f0e3b1acd55358a2e63f1e821
Stephen Hodgson <hodgson.designs@gmail.com>
2021-09-29 14:20:18 -0400

    add another line?

--- InstallUnityHub.ps1
+++ InstallUnityHub.ps1
@@ -80,7 +80,7 @@ elseif ($global:PSVersionTable.OS.Contains("Linux")) {
   chmod -v a+x "$hubInstallationPath"
   touch "$HOME/.config/Unity Hub/eulaAccepted"
   touch "$hubPath"
-  sudo echo "#!/bin/bash\nxvfb-run --auto-servernum `"`"$hubInstallationPath`"`" `"`"$@`"`"" > "$hubPath"
+  sudo echo "#!/bin/bash\n\nxvfb-run --auto-servernum `"`"$hubInstallationPath`"`" `"`"$@`"`"" > "$hubPath"
   echo "$(cat $hubPath)"
   sudo chmod -v a+x "$hubPath"
   # /UnityHub.AppImage --headless help
