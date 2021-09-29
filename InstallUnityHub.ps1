5aef0293c94fd582f87a6fa6f84031eaf646594b
Stephen Hodgson <hodgson.designs@gmail.com>
2021-09-29 13:10:53 -0400

    create base path

--- InstallUnityHub.ps1
+++ InstallUnityHub.ps1
@@ -67,7 +67,7 @@ elseif ($global:PSVersionTable.OS.Contains("Linux")) {
   $editorPath = "$HOME/Unity/Hub/Editor/"
   $editorFileEx = "Unity"
 
-  mkdir -pv "$HOME/Unity Hub" "$HOME/.config/Unity Hub"
+  mkdir -pv "$HOME/Unity Hub" "$HOME/.config/Unity Hub" "$editorPath"
   sudo apt-get update
   sudo apt-get install -y libgconf-2-4 libglu1 libasound2 libgtk2.0-0 libgtk-3-0 libnss3 zenity xvfb
 
