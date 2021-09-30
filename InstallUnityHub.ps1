ceedf31530b05b84ffba6f934157a04d89269e9d
Stephen Hodgson <hodgson.designs@gmail.com>
2021-09-29 22:33:46 -0400

    only install hub if needed. added modules expicitly

--- InstallUnityHub.ps1
+++ InstallUnityHub.ps1
@@ -1,138 +1,138 @@
+$version = "m_EditorVersionWithRevision: 2019.1.14f1 (148b5891095a)"
+
 Write-Host "$(Get-Date): Downloading Unity Hub..."
 
 $baseUrl = "https://public-cdn.cloud.unity3d.com/hub/prod";
 $outPath = $PSScriptRoot
-$version = "m_EditorVersionWithRevision: 2019.1.14f1 (148b5891095a)"
 $pattern = '(?<version>(?:(?<major>\d+)\.)?(?:(?<minor>\d+)\.)?(?:(?<patch>\d+[fab]\d+)\b))|((?:\((?<revision>\w+))\))'
 $versonMatches = [regex]::Matches($version, $pattern)
 $UnityVersion = $versonMatches[0].Groups['version'].Value.Trim()
 $UnityVersionChangeSet = $versonMatches[1].Groups['revision'].Value.Trim()
 
-$wc = New-Object System.Net.WebClient
 
-Write-Host "$(Get-Date): Download Complete, Starting installation..."
-
-if ((-not $global:PSVersionTable.Platform) -or ($global:PSVersionTable.Platform -eq "Win32NT")) {
-  $wc.DownloadFile("$baseUrl/UnityHubSetup.exe", "$outPath/UnityHubSetup.exe")
-  $startProcessArgs = @{
-    'FilePath'     = "$outPath/UnityHubSetup.exe";
-    'ArgumentList' = @('/S');
-    'PassThru'     = $true;
-    'Wait'         = $true;
-  }
-
-  # Run Installer
-  $process = Start-Process @startProcessArgs
-
-  if ( $process.ExitCode -ne 0) {
-    Write-Error "$(Get-Date): Failed with exit code: $($process.ExitCode)"
-    exit 1
-  }
-
+if ( (-not $global:PSVersionTable.Platform) -or ($global:PSVersionTable.Platform -eq "Win32NT") ) {
   $hubPath = "C:\Program Files\Unity Hub\Unity Hub.exe"
-  $editorPath = "C:\Program Files\Unity\Hub\Editor\"
-  $editorFileEx = "Editor\Unity.exe"
-
-  if ( -not (Test-Path "$hubPath") ) {
-    Write-Error "Unity Hub.exe path not found!"
-    exit 1
-  }
-
-  #"Unity Hub.exe" -- --headless help
-  #. 'C:\Program Files\Unity Hub\Unity Hub.exe' -- --headless help
-  function unity-hub {
-    $p = Start-Process -Verbose -NoNewWindow -PassThru -Wait -FilePath "$hubPath" -ArgumentList (@('--','--headless') + $args.Split(" "))
-  }
+  $editorRootPath = "C:\Program Files\Unity\Hub\Editor\"
+  $editorFileEx = "\Editor\Unity.exe"
+  $modules = @('windows-il2cpp', 'universal-windows-platform', 'lumin', 'webgl', 'android')
 }
-elseif ($global:PSVersionTable.OS.Contains("Darwin")) {
-  $package = "UnityHubSetup.dmg"
-  $downloadPath = "$outPath/$package"
-  $wc.DownloadFile("$baseUrl/$package", $downloadPath)
-
-  $dmgVolume = (sudo hdiutil attach $downloadPath -nobrowse) | Select-String -Pattern '\/Volumes\/.*' -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object { $_.Value } | select-object -first 1
-  Write-Host $dmgVolume
-  $dmgAppPath = (find "$DMGVolume" -name "*.app" -depth 1)
-  Write-Host $dmgAppPath
-  sudo cp -rf "`"$dmgAppPath`"" "/Applications"
-  hdiutil unmount $dmgVolume
-
+elseif ( $global:PSVersionTable.OS.Contains("Darwin") ) {
   $hubPath = "/Applications/Unity Hub.app/Contents/MacOS/Unity Hub"
-  $editorPath = "/Applications/Unity/Hub/Editor/"
-  $editorFileEx = "Unity.app"
-  # /Applications/Unity\ Hub.app/Contents/MacOS/Unity\ Hub -- --headless help
-  #. "/Applications/Unity Hub.app/Contents/MacOS/Unity Hub" -- --headless help
-  function unity-hub {
-    $p = Start-Process -Verbose -NoNewWindow -PassThru -Wait -FilePath "$hubPath" -ArgumentList (@('--','--headless') + $args.Split(" "))
-  }
+  $editorRootPath = "/Applications/Unity/Hub/Editor/"
+  $editorFileEx = "/Unity.app"
+  $modules = @('mac-il2cpp', 'ios', 'lumin', 'webgl', 'android')
 }
-elseif ($global:PSVersionTable.OS.Contains("Linux")) {
+elseif ( $global:PSVersionTable.OS.Contains("Linux") ) {
   $hubPath = "$HOME/Unity Hub/UnityHub.AppImage"
-  $editorPath = "$HOME/Unity/Hub/Editor/"
+  $editorRootPath = "$HOME/Unity/Hub/Editor/"
   $editorFileEx = "Editor/Unity"
+  $modules = @('linux', 'lumin', 'webgl', 'android')
+}
 
-  mkdir -pv "$HOME/Unity Hub" "$HOME/.config/Unity Hub" "$editorPath"
-  sudo apt-get update
-  sudo apt-get install -y libgconf-2-4 libglu1 libasound2 libgtk2.0-0 libgtk-3-0 libnss3 zenity xvfb
+if ( -not (Test-Path -Path "$hubPath") ) {
+  $wc = New-Object System.Net.WebClient
 
-  #https://www.linuxdeveloper.space/install-unity-linux/
-  $wc.DownloadFile("$baseUrl/UnityHub.AppImage", "$hubPath")
-  chmod -v a+x "$hubPath"
-  touch "$HOME/.config/Unity Hub/eulaAccepted"
+  Write-Host "$(Get-Date): Download Complete, Starting installation..."
 
-  # /UnityHub.AppImage --headless help
-  # xvfb-run --auto-servernum "$HOME/Unity Hub/UnityHub.AppImage" --headless help
-  function unity-hub {
-    xvfb-run --auto-servernum "$hubPath" --headless $args
+  if ((-not $global:PSVersionTable.Platform) -or ($global:PSVersionTable.Platform -eq "Win32NT")) {
+    $wc.DownloadFile("$baseUrl/UnityHubSetup.exe", "$outPath/UnityHubSetup.exe")
+    $startProcessArgs = @{
+      'FilePath'     = "$outPath/UnityHubSetup.exe";
+      'ArgumentList' = @('/S');
+      'PassThru'     = $true;
+      'Wait'         = $true;
+    }
+
+    # Run Installer
+    $process = Start-Process @startProcessArgs
+
+    if ( $process.ExitCode -ne 0) {
+      Write-Error "$(Get-Date): Failed with exit code: $($process.ExitCode)"
+      exit 1
+    }
+
+    #"Unity Hub.exe" -- --headless help
+    #. 'C:\Program Files\Unity Hub\Unity Hub.exe' -- --headless help
+    function unity-hub {
+      $p = Start-Process -Verbose -NoNewWindow -PassThru -Wait -FilePath "$hubPath" -ArgumentList (@('--','--headless') + $args.Split(" "))
+    }
+  }
+  elseif ($global:PSVersionTable.OS.Contains("Darwin")) {
+    $package = "UnityHubSetup.dmg"
+    $downloadPath = "$outPath/$package"
+    $wc.DownloadFile("$baseUrl/$package", $downloadPath)
+
+    $dmgVolume = (sudo hdiutil attach $downloadPath -nobrowse) | Select-String -Pattern '\/Volumes\/.*' -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object { $_.Value } | select-object -first 1
+    Write-Host $dmgVolume
+    $dmgAppPath = (find "$DMGVolume" -name "*.app" -depth 1)
+    Write-Host $dmgAppPath
+    sudo cp -rf "`"$dmgAppPath`"" "/Applications"
+    hdiutil unmount $dmgVolume
+
+    # /Applications/Unity\ Hub.app/Contents/MacOS/Unity\ Hub -- --headless help
+    #. "/Applications/Unity Hub.app/Contents/MacOS/Unity Hub" -- --headless help
+    function unity-hub {
+      $p = Start-Process -Verbose -NoNewWindow -PassThru -Wait -FilePath "$hubPath" -ArgumentList (@('--','--headless') + $args.Split(" "))
+    }
+  }
+  elseif ($global:PSVersionTable.OS.Contains("Linux")) {
+
+    mkdir -pv "$HOME/Unity Hub" "$HOME/.config/Unity Hub" "$editorRootPath"
+    sudo apt-get update
+    sudo apt-get install -y libgconf-2-4 libglu1 libasound2 libgtk2.0-0 libgtk-3-0 libnss3 zenity xvfb
+
+    #https://www.linuxdeveloper.space/install-unity-linux/
+    $wc.DownloadFile("$baseUrl/UnityHub.AppImage", "$hubPath")
+    chmod -v a+x "$hubPath"
+    touch "$HOME/.config/Unity Hub/eulaAccepted"
+
+    # /UnityHub.AppImage --headless help
+    # xvfb-run --auto-servernum "$HOME/Unity Hub/UnityHub.AppImage" --headless help
+    function unity-hub {
+      xvfb-run --auto-servernum "$hubPath" --headless $args
+    }
   }
 }
 
-Write-Host "Install Hub Complete: $hubPath"
+if ( -not (Test-Path "$hubPath") ) {
+  Write-Error "$hubPath path not found!"
+  exit 1
+}
+
+Write-Host "Unity Hub found at `"$hubPath`""
 Write-Host ""
-Write-Host "Unity HUB CLI Options:"
+
+Write-Host "Editor root path currently set to: `"$editorRootPath`""
+Write-Host ""
+
 unity-hub help
 Write-Host ""
 
-$modulesPath = "$editorPath$UnityVersion"
-$editorPath = '{0}{1}{2}' -f $modulesPath,[IO.Path]::DirectorySeparatorChar,$editorFileEx
+$editorPath = "{0}{1}{2}" -f $editorRootPath,$unityVersion,$editorFileEx
 
 if ( -not (Test-Path -Path $editorPath) ) {
-  if ( Test-Path -Path $modulesPath ) {
-    $modulesPath = '{0}{1}modules.json' -f $modulesPath,[IO.Path]::DirectorySeparatorChar
+  Write-Host "Installing $unityVersion ($unityVersionChangeSet)..."
+  Write-Host ""
+  $installArgs = @('install',"--version $unityVersion","--changeset $unityVersionChangeSet",'--cm')
 
-    if ( Test-Path -Path $modulesPath ) {
-      Write-Host "Modules Manifest: "$modulesPath
-      $installArgs = @('install',"--version $unityVersion","--changeset $unityVersionChangeSet",'--cm')
-
-      foreach ($module in (Get-Content -Raw -Path $modulesPath | ConvertFrom-Json)) {
-        if ( ($module.category -eq 'Platforms') -and ($module.visible -eq $true) ) {
-          $installArgs += '-m'
-          $installArgs += $module.id
-          Write-Host ""
-          Write-Host "found platform module" $module.id
-        }
-      }
-
-      $installArgsString = $installArgs -join " "
-
-      Write-Host ""
-      unity-hub $installArgsString
-      Write-Host ""
-      unity-hub editors -i
-      Write-Host ""
-    } else {
-      Write-Error "Failed to resolve modules path at $modulesPath"
-      exit 1
-    }
-  } else {
-    Write-Error "Failed to resolve editor installation path at $modulesPath"
-    exit 1
+  foreach ( $module in $modules ) {
+    $installArgs += '-m'
+    $installArgs += $module
+    Write-Host " + adding module: $module"
   }
+
+  $installArgsString = $installArgs -join " "
+
+  unity-hub $installArgsString
+  Write-Host ""
+  unity-hub editors -i
+  Write-Host ""
 }
+
 if ( -not (Test-Path -Path $editorPath) ) {
   Write-Error "Failed to validate installed editor path at $editorPath"
   exit 1
 }
 
-Write-Host "Install Complete!"
 Write-Host "UnityEditor path set to: $editorPath"
 exit 0
